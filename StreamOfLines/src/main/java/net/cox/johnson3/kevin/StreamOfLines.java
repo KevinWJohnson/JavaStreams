4f8c21066003e58d65de6652f340ea26807663dd
Kevin Johnson <kevin.johnson3@cox.net>
2020-09-01 13:05:36 -0700

    Added program to create a Stream from a file.

---
+++ StreamOfLines/src/main/java/net/cox/johnson3/kevin/StreamOfLines.java
@@ -0,0 +1,40 @@
+package net.cox.johnson3.kevin;
+
+// Fig. 17.22: StreamOfLines.java
+// Counting word occurrences in a text file.
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class StreamOfLines
+{
+    public static void main( String[] args ) throws IOException
+    {
+        // Regex that matches one or more consecutive whitespace characters
+        final Pattern pattern = Pattern.compile("\\s+");
+
+        // count occurrences of each word in a Stream<String> sorted by word
+        Map<String, Long> wordCounts =
+                Files.lines(Paths.get("C:\\Users\\kevin\\IdeaProjects\\StreamPractice\\StreamOfLines\\src\\main\\java\\net\\cox\\johnson3\\kevin\\StreamOfLinesInput.txt"))
+                        .map(line -> line.replaceAll("\\p{Punct}", ""))
+                        .flatMap(line -> pattern.splitAsStream(line))
+                        .collect(Collectors.groupingBy(String::toLowerCase,
+                            TreeMap::new, Collectors.counting()));
+
+        // display the words grouped by starting letter
+        wordCounts.entrySet()
+                .stream()
+                .collect(
+                        Collectors.groupingBy(entry -> entry.getKey().charAt(0),
+                                TreeMap::new, Collectors.toList()))
+                .forEach((letter, wordList) -> {
+                    System.out.printf("%n%C%n", letter);
+                    wordList.stream().forEach(word -> System.out.printf(
+                            "%13s: %d%n", word.getKey(), word.getValue()));
+                });
+    }
+}
